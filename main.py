4b8d2f45fbb5438fcd49f80bdbc2cd0379e4bbe6
preranaiyengar <preranaiyengar26@gmail.com>
2026-09-04 13:16:30 +0530

    Allow Vercel frontend in CORS

--- main.py
+++ main.py
@@ -27,11 +27,12 @@ app = FastAPI(title="FindMe API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://document-insight-engine-frontend.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
